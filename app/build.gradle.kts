605d01e9e82f0b3e8619475f41937ec60e3c1743
Ridvan Ozdemir <66924366+ridvanozdemir@users.noreply.github.com>
2026-08-28 13:49:04 +0300

    Rollback to stable 1.1.0 code as Play update 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.ridvanozdemir.planim"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.1.2"
+        versionCode = 8
+        versionName = "1.1.3"
     }
 
     signingConfigs {
