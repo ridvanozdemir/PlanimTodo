566e2319191bdfa2ebb912c453d3e772ad927c56
Ridvan Ozdemir <66924366+ridvanozdemir@users.noreply.github.com>
2026-08-28 12:35:14 +0300

    Bump Planim to 1.1.2 stability release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.ridvanozdemir.planim"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.1.1"
+        versionCode = 7
+        versionName = "1.1.2"
     }
 
     signingConfigs {
