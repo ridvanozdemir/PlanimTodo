d1beb6e0a77cd894b384b276b3718af91d2fa9eb
Ridvan Ozdemir <66924366+ridvanozdemir@users.noreply.github.com>
2026-09-05 19:47:26 +0300

    Bump Planım to 1.1.6 (11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.ridvanozdemir.planim"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.1.5"
+        versionCode = 11
+        versionName = "1.1.6"
     }
 
     signingConfigs {
