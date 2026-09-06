805b3d75315dd7133e7ac6752b50e34f5fb50ae2
Ridvan Ozdemir <66924366+ridvanozdemir@users.noreply.github.com>
2026-09-06 18:28:05 +0300

    Update version code and name to 13 and 1.1.8
    bump versionCode to 13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.ridvanozdemir.planim"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.1.7"
+        versionCode = 13
+        versionName = "1.1.8"
     }
 
     signingConfigs {
